33abfbc7b18997602736d787eae1cd9a257b5e23
Sho Fujiwara <shofujiwara505@gmail.com>
2023-05-29 08:40:58 +0900

    put the api function in the same file as SEarch

--- src/api/CocktailApi.jsx
+++ src/api/CocktailApi.jsx
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react";
-import Search from "../components/Search";
+// import { useState, useEffect } from "react";
+// import Search from "../components/Search";
 
-export function CocktailApi({ searchedCocktailName }) {
-  const [cocktails, setCocktails] = useState([]);
-  useEffect(() => {
-    fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${searchedCocktailName}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const drinks = data.drinks.map((drink) => {
-          return { cocktail: drink.strDrink, key: `${drink.strDrink}Cocktail` };
-        });
-        setCocktails(drinks && drinks);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    console.log(cocktails);
-  }, [searchedCocktailName]);
-}
+// export function CocktailApi({ searchedCocktailName }) {
+//   const [cocktails, setCocktails] = useState([]);
+//   useEffect(() => {
+//     fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${searchedCocktailName}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const drinks = data.drinks.map((drink) => {
+//           return { cocktail: drink.strDrink, key: `${drink.strDrink}Cocktail` };
+//         });
+//         setCocktails(drinks && drinks);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//     console.log(cocktails);
+//   }, [searchedCocktailName]);
+// }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
-import { CocktailApi } from "../api/CocktailApi";
+import Select from "react-select";
+import AsyncSelect from "react-select/async";
+import { useEffect } from "react";
 
-export default function Search({ cocktails }) {
+export default function Search() {
   const [cocktail, setCocktail] = useState("");
+  const [example, setExample] = useState(["bonon", "app", "wow", "wowowo"]);
+
+  const loadOptions = (cocktail) => {
+    setExample([...example, cocktail]);
+  };
 
   const handleCocktailSearch = (e) => {
     setCocktail(e.target.value);
@@ -11,10 +18,33 @@ export default function Search({ cocktails }) {
   const handleCocktailSearchClick = (e) => {
     e.preventDefault();
   };
+
+  const [cocktails, setCocktails] = useState([]);
+  useEffect(() => {
+    fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const drinks = data.drinks.map((drink) => {
+          return { cocktail: drink.strDrink, key: `${drink.strDrink}Cocktail` };
+        });
+        setCocktails(drinks && drinks);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log("cocktail result", cocktails ? cocktails : "not found");
+  }, [cocktail]);
+
   return (
     <div>
       <h1 className="text-center text-4xl">Search here</h1>
       <form>
+        <AsyncSelect
+          loadOptions={loadOptions}
+          placeholder="Search for cocktails"
+          value={cocktail}
+          onChange={handleCocktailSearch}
+        />
         <input
           type="dropbox"
           placeholder="Search for cocktails"
@@ -26,8 +56,6 @@ export default function Search({ cocktails }) {
           Submit
         </button>
       </form>
-
-      <CocktailApi searchedCocktailName={cocktail} />
     </div>
   );
 }
