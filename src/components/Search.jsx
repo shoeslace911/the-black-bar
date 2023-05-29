2236876d8539737758fb64e8db0f11deca89bdbf
Sho Fujiwara <shofujiwara505@gmail.com>
2023-05-29 09:15:44 +0900

    async now works

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,57 +1,40 @@
+import React from "react";
 import { useState } from "react";
-import Select from "react-select";
 import AsyncSelect from "react-select/async";
 import { useEffect } from "react";
 
 export default function Search() {
   const [cocktail, setCocktail] = useState("");
-  const [example, setExample] = useState(["bonon", "app", "wow", "wowowo"]);
-
-  const loadOptions = (cocktail) => {
-    setExample([...example, cocktail]);
-  };
 
   const handleCocktailSearch = (e) => {
     setCocktail(e.target.value);
+    console.log("cocktail value", cocktail);
   };
 
   const handleCocktailSearchClick = (e) => {
     e.preventDefault();
   };
 
-  const [cocktails, setCocktails] = useState([]);
-  useEffect(() => {
-    fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const drinks = data.drinks.map((drink) => {
-          return { cocktail: drink.strDrink, key: `${drink.strDrink}Cocktail` };
-        });
-        setCocktails(drinks && drinks);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    console.log("cocktail result", cocktails ? cocktails : "not found");
-  }, [cocktail]);
+  const loadOptions = async () => {
+    try {
+      const response = await fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`);
+      const data = await response.json();
+
+      const drinks = data.drinks.map((drink) => ({
+        value: drink.strDrink,
+        label: drink.strDrink,
+      }));
+      return drinks;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <h1 className="text-center text-4xl">Search here</h1>
       <form>
-        <AsyncSelect
-          loadOptions={loadOptions}
-          placeholder="Search for cocktails"
-          value={cocktail}
-          onChange={handleCocktailSearch}
-        />
-        <input
-          type="dropbox"
-          placeholder="Search for cocktails"
-          onChange={handleCocktailSearch}
-          value={cocktail}
-          className=" bg-slate-300"
-        />
+        <AsyncSelect loadOptions={loadOptions} placeholder="Search for cocktails" onChange={handleCocktailSearch} />
         <button type="drop" onClick={handleCocktailSearchClick}>
           Submit
         </button>
