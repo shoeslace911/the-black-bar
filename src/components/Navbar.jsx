40db6c027220dbbb0a52ed505d935cbd6c8094cc
Sho Fujiwara <shofujiwara505@gmail.com>
2023-05-30 15:39:28 +0900

    got fonts working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <nav>
-      <ul className="flex gap-5 text-5xl bg-slate-400 p-5 justify-evenly">
+      <ul className="flex gap-5 text-5xl bg-slate-400 p-5 justify-evenly font-display">
         <a href="#">
           <li>Home</li>
         </a>
