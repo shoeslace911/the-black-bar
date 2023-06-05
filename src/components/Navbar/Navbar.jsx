08c449bfe414f5fa6ed92cc8d737cf745dbd9093
Sho Fujiwara <shofujiwara505@gmail.com>
2023-06-05 13:50:30 +0900

    almost done, theres a problem when you click the drink u want. The text color in the textbox becomes black and hard to read

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -34,6 +34,26 @@ export default function Navbar() {
       ...past,
       color: "white",
     }),
+    input: (provided) => ({
+      ...provided,
+      color: "white",
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected && "silver", // Change the background color of selected and non-selected options
+      color: state.isSelected && "black",
+      "&:hover": {
+        backgroundColor: "lightgray", // Change the background color on hover
+        color: "black", // Change the text color on hover
+      },
+    }),
+    menu: (provided, state) => ({
+      ...provided,
+      backgroundColor: "none",
+      color: "white",
+      borderRadius: "4px",
+      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    }),
   };
 
   return (
@@ -54,7 +74,7 @@ export default function Navbar() {
         <Searchbar
           styles={searchbarStyle}
           onChange={handleOnChange}
-          className={" w-72 text-xl text-black "}
+          className={" w-72 text-xl text-black  "}
           placeholder={`Cocktails`}
         />
       </ul>
