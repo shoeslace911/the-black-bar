e800cb6bd15cdfbd4559954ceb5749481f38b4f2
Maciek Sitkowski <58401630+sitek94@users.noreply.github.com>
2023-06-21 17:02:21 +0000

    improve landing page contrast
    - use separate div for background layer, to prevent opacity applying to h1 and searchbar
    - improve contrast by applying grayscale and opacity

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -17,14 +17,17 @@ export default function Search() {
   // react-select async searchbar style
 
   return (
-    <div className="flex flex-col justify-center h-screen bg-hero bg-cover font-display ">
-      <h1 className="text-center text-8xl drop-shadow-lg brightness-100 text-white">SEARCH FOR COCKTAILS</h1>
-      <Searchbar
-        onChange={handleOnChange}
-        placeholder={"Cocktails"}
-        className={"bg-grey-300 w-4/12 pt-5 mx-auto text-2xl "}
-        styles={SearchBarStyle}
-      />
+    <div className="h-screen flex flex-col justify-center">
+      <div className="absolute inset-0 bg-hero bg-cover grayscale opacity-25" />
+      <div className="font-display">
+        <h1 className="text-center text-8xl drop-shadow-lg brightness-100 text-white">SEARCH FOR COCKTAILS</h1>
+        <Searchbar
+          onChange={handleOnChange}
+          placeholder={"Cocktails"}
+          className={"bg-grey-300 w-4/12 pt-5 mx-auto text-2xl "}
+          styles={SearchBarStyle}
+          />
+      </div>
     </div>
   );
 }
